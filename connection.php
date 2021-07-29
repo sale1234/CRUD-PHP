ae9e951bfec1aec7771944dd7b68b266062c642f
sale1234 <jevremovicsasa@gmail.com>
2021-07-29 19:46:44 +0200

    connection page added

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+
+  $con = mysqli_connect('localhost', 'root', '', 'datatable_db');
+  if(mysqli_connect_errno())
+  {
+    echo "Database connection error!";
+    exit;
+
+  }
--- index.php
+++ index.php
@@ -51,7 +51,26 @@
     <!-- Bootstrap script link -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script type="text/javascript">
-      $('#datatable').DataTable({})
+      // new look for table
+      
+      $('#datatable').DataTable({
+        "fnCreatedRow": function( nRow, aData, iDataIndex ) {
+          $(nRow).attr('id', aData[0]);
+        },
+        'serverSide':'true',
+        'processing':'true',
+        'paging':'true',
+        'order':[],
+        'ajax': {
+          'url':'fetch_data.php',
+          'type':'post',
+        },
+        "columnDefs": [{
+          'target':[5],
+          'orderable' :false,
+        }]
+      });
+    
     </script>
 
   </body>
