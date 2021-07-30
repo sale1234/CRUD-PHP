8d5de3cd6aeea1936fd5ebbd9665498b2028705f
sale1234 <jevremovicsasa@gmail.com>
2021-07-30 18:24:23 +0200

    finally users added

--- index.php
+++ index.php
@@ -1,66 +1,65 @@
 <?php include('connection.php');?>
 <!doctype html>
 <html lang="en">
-  <head>
-    
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-   <!-- Bootstrap cnd -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/jq-3.3.1/dt-1.10.25/datatables.min.css"/>
- 
-
-    <title>CRUD PHP MYSQL AJAX</title>
-  </head>
-  <body>
-   <h1 class="text-center">PHP Project</h1>
-   <div class="container-fluid">
-     <div class="row">
-       <div class="container">
-         <div class="row">
-         <div class="col-md-2"></div>
-           <div class="col-md-8">
-             <!-- Button trigger modal -->
-              <button type="button" style="margin-bottom: 30px" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addUserModal">
-                Add User
-              </button>
-           </div>
-         </div>
-         <div class="row">
-           <div class="col-md-2"></div>
-           <div class="col-md-8">
-             <table id="datatable" class="table">
-               <thead>
-                 <th>ID</th>
-                 <th>Username</th>
-                 <th>Email</th>
-                 <th>Mobile</th>
-                 <th>City</th>
-                 <th>Options</th>
-               </thead>
-               <tbody>
-                
-               </tbody>
-             </table>
-           </div>
-         </div>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.10.25/af-2.3.7/date-1.1.0/r-2.2.9/rg-1.1.3/sc-2.0.4/sp-1.3.0/datatables.min.css"/>
+  <title>Server Side CRUD Ajax Operations</title>
+  <style type="text/css">
+    .btnAdd {
+      text-align: right;
+      width: 83%;
+      margin-bottom: 20px;
+    }
+  </style>
+</head>
+<body>
+  <div class="container-fluid">
+    <h2 class="text-center">DataTable</h2>
+    <p class="datatable design text-center">Welcome to Datatable</p>
+    <div class="row">
+      <div class="container">
+        <div class="btnAdd">
+         <a href="#!" data-id="" data-bs-toggle="modal" data-bs-target="#addUserModal"   class="btn btn-success btn-sm" >Add User</a>
        </div>
-     </div>
-   </div>
-
-    <!-- jQuery script link -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <!-- DataTable link -->
-    <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/jq-3.3.1/dt-1.10.25/datatables.min.js"></script>
-    <!-- Bootstrap script link -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    
-    <script type="text/javascript">
-      // new look for table
-      
-      $(document).ready(function() {
-      $('#datatable').DataTable({
+       <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+         <table id="example" class="table">
+          <thead>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Mobile</th>
+            <th>City</th>
+            <th>Options</th>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
+      </div>
+      <div class="col-md-2"></div>
+    </div>
+  </div>
+</div>
+</div>
+<!-- Optional JavaScript; choose one of the two! -->
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.10.25/af-2.3.7/date-1.1.0/r-2.2.9/rg-1.1.3/sc-2.0.4/sp-1.3.0/datatables.min.js"></script>
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+  -->
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $('#example').DataTable({
         "fnCreatedRow": function( nRow, aData, iDataIndex ) {
           $(nRow).attr('id', aData[0]);
         },
@@ -78,29 +77,27 @@
         }]
       });
     } );
-    
-      $(document).on('submit','#saveUserForm',function(e){
+    $(document).on('submit','#addUser',function(e){
       e.preventDefault();
-      var username= $('#inputUsername').val();
-      var email= $('#inputEmail').val();
-      var mobile= $('#inputMobile').val();
-      var city= $('#inputCity').val();
+      var city= $('#addCityField').val();
+      var username= $('#addUserField').val();
+      var mobile= $('#addMobileField').val();
+      var email= $('#addEmailField').val();
       if(city != '' && username != '' && mobile != '' && email != '' )
       {
        $.ajax({
          url:"add_user.php",
+         type:"post",
          data:{city:city,username:username,mobile:mobile,email:email},
-         type:'post',
          success:function(data)
          {
            var json = JSON.parse(data);
            var status = json.status;
-           if(status == 'success')
+           if(status=='true')
            {
-            table = $('#datatable').DataTable();
-            table.draw();
-            alert('Successfully user added');
-            // $('#addUserModal').modal('hide');
+            mytable =$('#example').DataTable();
+            mytable.draw();
+            $('#addUserModal').modal('hide');
           }
           else
           {
@@ -113,51 +110,205 @@
       alert('Fill all the required fields');
     }
   });
-    </script>
+    // $(document).on('submit','#updateUser',function(e){
+    //   e.preventDefault();
+    //    //var tr = $(this).closest('tr');
+    //    var city= $('#cityField').val();
+    //    var username= $('#nameField').val();
+    //    var mobile= $('#mobileField').val();
+    //    var email= $('#emailField').val();
+    //    var trid= $('#trid').val();
+    //    var id= $('#id').val();
+    //    if(city != '' && username != '' && mobile != '' && email != '' )
+    //    {
+    //      $.ajax({
+    //        url:"update_user.php",
+    //        type:"post",
+    //        data:{city:city,username:username,mobile:mobile,email:email,id:id},
+    //        success:function(data)
+    //        {
+    //          var json = JSON.parse(data);
+    //          var status = json.status;
+    //          if(status=='true')
+    //          {
+    //           table =$('#example').DataTable();
+    //           // table.cell(parseInt(trid) - 1,0).data(id);
+    //           // table.cell(parseInt(trid) - 1,1).data(username);
+    //           // table.cell(parseInt(trid) - 1,2).data(email);
+    //           // table.cell(parseInt(trid) - 1,3).data(mobile);
+    //           // table.cell(parseInt(trid) - 1,4).data(city);
+    //           var button =   '<td><a href="javascript:void();" data-id="' +id + '" class="btn btn-info btn-sm editbtn">Edit</a>  <a href="#!" data-bs-toggle="modal" data-id="' +id + '" data-bs-target="#exampleModal" class="btn btn-danger btn-sm">Delete</a></td>';
+    //           var row = table.row("[id='"+trid+"']");
+    //           row.row("[id='" + trid + "']").data([id,username,email,mobile,city,button]);
+    //           $('#exampleModal').modal('hide');
+    //         }
+    //         else
+    //         {
+    //           alert('failed');
+    //         }
+    //       }
+    //     });
+    //    }
+    //    else {
+    //     alert('Fill all the required fields');
+    //   }
+    // });
+  //   $('#example').on('click','.editbtn ',function(event){
+  //     var table = $('#example').DataTable();
+  //     var trid = $(this).closest('tr').attr('id');
+  //    // console.log(selectedRow);
+  //    var id = $(this).data('id');
+  //    $('#exampleModal').modal('show');
 
-  <!-- Add user modal -->
-  <div class="modal fade" id="addUserModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">New User</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <form id="saveUserForm" action="javascript:void();" method = 'post'>
-          <div class="modal-body">
-            <div class="mb-3 row">
-              <label for="inputUsername" class="col-sm-2 col-form-label">Username</label>
-                <div class="col-sm-10">
-                  <input type="text" name="username" class="form-control" id="inputUsername" >
-                </div>
-            </div>
-            <div class="mb-3 row">
-              <label for="inputPassword" class="col-sm-2 col-form-label">Email</label>
-              <div class="col-sm-10">
-                <input type="text" name="email" class="form-control" id="inputEmail">
-              </div>
-            </div>
-            <div class="mb-3 row">
-              <label for="inputMobile" class="col-sm-2 col-form-label">Mobile</label>
-              <div class="col-sm-10">
-                <input type="text" name="mobile" class="form-control" id="inputMobile">
-              </div>
-            </div>
-            <div class="mb-3 row">
-              <label for="inputCity" class="col-sm-2 col-form-label">City</label>
-              <div class="col-sm-10">
-                <input type="text" name="city" class="form-control" id="inputCity">
-              </div>
+  //    $.ajax({
+  //     url:"get_single_data.php",
+  //     data:{id:id},
+  //     type:'post',
+  //     success:function(data)
+  //     {
+  //      var json = JSON.parse(data);
+  //      $('#nameField').val(json.username);
+  //      $('#emailField').val(json.email);
+  //      $('#mobileField').val(json.mobile);
+  //      $('#cityField').val(json.city);
+  //      $('#id').val(id);
+  //      $('#trid').val(trid);
+  //    }
+  //  })
+  //  });
+
+  //   $(document).on('click','.deleteBtn',function(event){
+  //      var table = $('#example').DataTable();
+  //     event.preventDefault();
+  //     var id = $(this).data('id');
+  //     if(confirm("Are you sure want to delete this User ? "))
+  //     {
+  //     $.ajax({
+  //       url:"delete_user.php",
+  //       data:{id:id},
+  //       type:"post",
+  //       success:function(data)
+  //       {
+  //         var json = JSON.parse(data);
+  //         status = json.status;
+  //         if(status=='success')
+  //         {
+  //           //table.fnDeleteRow( table.$('#' + id)[0] );
+  //            //$("#example tbody").find(id).remove();
+  //            //table.row($(this).closest("tr")) .remove();
+  //            $("#"+id).closest('tr').remove();
+  //         }
+  //         else
+  //         {
+  //           alert('Failed');
+  //           return;
+  //         }
+  //       }
+  //     });
+  //     }
+  //     else
+  //     {
+  //       return null;
+  //     }
+
+
+
+  //   })
+ </script>
+ <!-- Modal -->
+ <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Update User</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form id="updateUser" >
+          <input type="hidden" name="id" id="id" value="">
+          <input type="hidden" name="trid" id="trid" value="">
+          <div class="mb-3 row">
+            <label for="nameField" class="col-md-3 form-label">Name</label>
+            <div class="col-md-9">
+              <input type="text" class="form-control" id="nameField" name="name" >
             </div>
           </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="submit" class="btn btn-primary">Save changes</button>
+          <div class="mb-3 row">
+            <label for="emailField" class="col-md-3 form-label">Email</label>
+            <div class="col-md-9">
+              <input type="email" class="form-control" id="emailField" name="email">
+            </div>
           </div>
-        </form>
+          <div class="mb-3 row">
+            <label for="mobileField" class="col-md-3 form-label">Mobile</label>
+            <div class="col-md-9">
+              <input type="text" class="form-control" id="mobileField" name="mobile">
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label for="cityField" class="col-md-3 form-label">City</label>
+            <div class="col-md-9">
+              <input type="text" class="form-control" id="cityField" name="City">
+            </div>
+          </div>
+          <div class="text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form> 
+      </div>
+      <div class="modal-footer">
+        <button type="button"  class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
       </div>
     </div>
   </div>
-  <!-- End of modal -->
-  </body>
+</div>
+<!-- Add user Modal -->
+<div class="modal fade" id="addUserModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add User</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form id="addUser" action="">
+          <div class="mb-3 row">
+            <label for="addUserField" class="col-md-3 form-label">Name</label>
+            <div class="col-md-9">
+              <input type="text" class="form-control" id="addUserField" name="name" >
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label for="addEmailField" class="col-md-3 form-label">Email</label>
+            <div class="col-md-9">
+              <input type="email" class="form-control" id="addEmailField" name="email">
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label for="addMobileField" class="col-md-3 form-label">Mobile</label>
+            <div class="col-md-9">
+              <input type="text" class="form-control" id="addMobileField" name="mobile">
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label for="addCityField" class="col-md-3 form-label">City</label>
+            <div class="col-md-9">
+              <input type="text" class="form-control" id="addCityField" name="City">
+            </div>
+          </div>
+          <div class="text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form> 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
 </html>
+<script type="text/javascript">
+  //var table = $('#example').DataTable();
+</script>
